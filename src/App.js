e8eabffc3722297a839f3e6b2d33fba6913e5a6f
SS Jones <SSJones.code@gmail.com>
2020-10-07 13:10:01 -0700

    I renamed nav-bar to header per instructions and started the Header component. I imported the three components to the app.js inside of the src dir as Tucker did in class and I imported the three componenets files to the app.js in src as well. I exported the Header from Header and I also inported react from react and app.css.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Header />;
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Header(props){
+    return <h1>Hello from the Header!</h1>
+} 
