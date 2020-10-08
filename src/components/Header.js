37fbb8182d33330096819c4d3c74583c075e8568
SS Jones <SSJones.code@gmail.com>
2020-10-08 11:07:44 -0700

    I created the pages dir inside of the src dir per Becky. I then added what amount to views in handlebars in the pages in the form of four pages; Gallery.js, Home.js, Introduction.js, and Skills.js. I also placed the nav in container, row, cols and am trying to center that and the main page without the need of relative and absolute values and instead with justify content center.

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,29 +12,40 @@ export default function Header(props) {
 function Navigation() {
   return (
     <>
-      <Navbar className="color-nav container"
+    <div className="container">
+      <Navbar className="color-nav"
         sticky="top"
         collapseOnSelect
         expand="lg"
       >
+        <div className="row justify-content-center">
+          <div className="col">
         <Navbar.Brand href="#home"><h3 className="text-center text-shadow-light-font-light">Welcome</h3></Navbar.Brand>
+        </div>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#features"><h3 className="text-shadow-light-font-light">Introduction</h3></Nav.Link>
-            <Nav.Link href="#pricing"><h3 className="text-shadow-light-font-light">Skills</h3></Nav.Link>
-            <Nav.Link href="#pricing"><h3 className="text-shadow-light-font-light">Gallery</h3></Nav.Link>
+            <Nav.Link href="#features"><h3 className="col text-shadow-light-font-light">Introduction</h3></Nav.Link>
+            <Nav.Link href="#pricing"><h3 className="col text-shadow-light-font-light">Skills</h3></Nav.Link>
+
+            <Nav.Link href="#pricing"><h3 className="col text-shadow-light-font-light">Gallery</h3></Nav.Link>
+            <div className="col justify-content-center">
             <NavDropdown className="text-shadow-light-font-light" title="More Below" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1"><h1 className="link-text text-shadow-dark">Resume</h1></NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2"><h1 className="link-text text-shadow-dark">GITHUB</h1></NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3"><h1 className="link-text text-shadow-dark">LinkedIn</h1></NavDropdown.Item>
             </NavDropdown>
+            </div>
+            <div className="col justify-content-right">
             <span className="text-shadow-light-font-light-small">Phone: (303)880-9518 |</span>
           <br />
           <span className="text-shadow-light-font-light-small">Email: ssjones.code@gmail.com</span>
+          </div>
           </Nav>
         </Navbar.Collapse>
+        </div>
       </Navbar>
+      </div>
     </>
   );
 }
