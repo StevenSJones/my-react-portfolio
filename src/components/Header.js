2da51a4c1211614ba8311ec521796b8a46dd046b
SS Jones <SSJones.code@gmail.com>
2020-10-08 00:04:49 -0700

    I now need to center the elements on the page and finish the Welcome Page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ export default function Header(props) {
 function Navigation() {
   return (
     <>
-      <Navbar className="color-nav"
+      <Navbar className="color-nav container"
         sticky="top"
         collapseOnSelect
         expand="lg"
@@ -21,7 +21,7 @@ function Navigation() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#features"><h3 className="text-shadow-light-font-light">About Me</h3></Nav.Link>
+            <Nav.Link href="#features"><h3 className="text-shadow-light-font-light">Introduction</h3></Nav.Link>
             <Nav.Link href="#pricing"><h3 className="text-shadow-light-font-light">Skills</h3></Nav.Link>
             <Nav.Link href="#pricing"><h3 className="text-shadow-light-font-light">Gallery</h3></Nav.Link>
             <NavDropdown className="text-shadow-light-font-light" title="More Below" id="collasible-nav-dropdown">
@@ -29,6 +29,9 @@ function Navigation() {
               <NavDropdown.Item href="#action/3.2"><h1 className="link-text text-shadow-dark">GITHUB</h1></NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3"><h1 className="link-text text-shadow-dark">LinkedIn</h1></NavDropdown.Item>
             </NavDropdown>
+            <span className="text-shadow-light-font-light-small">Phone: (303)880-9518 |</span>
+          <br />
+          <span className="text-shadow-light-font-light-small">Email: ssjones.code@gmail.com</span>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
