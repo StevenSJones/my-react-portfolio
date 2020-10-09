7e98257565c6ea687f69038c545949a2a7c5f080
SS Jones <SSJones.code@gmail.com>
2020-10-08 17:07:32 -0700

    Installed update through cli, and added links to the nav.

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,15 +20,15 @@ function Navigation() {
       >
         <div className="row justify-content-center">
           <div className="col">
-        <Navbar.Brand href="#home"><h3 className="text-center text-shadow-light-font-light">Welcome</h3></Navbar.Brand>
+        <Navbar.Brand href="/home"><h3 className="text-center text-shadow-light-font-light">Welcome</h3></Navbar.Brand>
         </div>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#features"><h3 className="col text-shadow-light-font-light">Introduction</h3></Nav.Link>
-            <Nav.Link href="#pricing"><h3 className="col text-shadow-light-font-light">Skills</h3></Nav.Link>
+            <Nav.Link href="/introduction"><h3 className="col text-shadow-light-font-light">Introduction</h3></Nav.Link>
+            <Nav.Link href="/skills"><h3 className="col text-shadow-light-font-light">Skills</h3></Nav.Link>
 
-            <Nav.Link href="#pricing"><h3 className="col text-shadow-light-font-light">Gallery</h3></Nav.Link>
+            <Nav.Link href="/gallery"><h3 className="col text-shadow-light-font-light">Gallery</h3></Nav.Link>
             <div className="col justify-content-center">
             <NavDropdown className="text-shadow-light-font-light" title="More Below" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1"><h1 className="link-text text-shadow-dark">Resume</h1></NavDropdown.Item>
