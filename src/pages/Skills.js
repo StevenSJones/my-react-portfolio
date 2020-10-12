54d122efa4b9e60200a3a1ab8e78a67cebfaaf63
SS Jones <SSJones.code@gmail.com>
2020-10-11 22:40:13 -0700

    I had the skills page proofread as well and made some ammendments in response.

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -17,11 +17,10 @@ function Skills() {
           <Card.Body>
             <Card.Title>Stackoverflow </Card.Title>
             <Card.Text>
-              Whenever I encountered an error the initial site that I would see
-              when I went to my favorite browser to perform a search would
-              innevitably be a similar issue resolved (must have the green check
+              Whenever I encountered an error during coding the initial answer that I would find
+              when I went to my favorite browser to perform a search would be a similar issue resolved (must have the green check
               mark that indicates that the issue is resolved or else they likely
-              have no answers for you, trust me) on Stackoverflow.com.
+              have no answers for you, trust me) on Stackoverflow.com. 
             </Card.Text>
           </Card.Body>
         </Card>
@@ -35,10 +34,10 @@ function Skills() {
           <Card.Body>
             <Card.Title>w3schools</Card.Title>
             <Card.Text>
-              I found w3schools to be invaluable in my daily problem solving and
+              W3schools is a website that I found w3schools to be invaluable in my daily problem solving and
               concept recpaps, both of which I performed daily during the
               program. The site is very well organized and easy to follow which
-              makes it ideal for beginners.
+              makes it ideal for beginners and those well versed looking for a quick refresh.
             </Card.Text>
           </Card.Body>
         </Card>
@@ -53,9 +52,9 @@ function Skills() {
           <Card.Body>
             <Card.Title>Slack</Card.Title>
             <Card.Text>
-              Slack is used by myself to communicate with other classmates or
-              team members or coworkers once I transition to a career in web
-              development. I used it daily and would've been lost without it.
+              Slack is a communcation app used by myself and many others daily to communicate with other classmates,
+              team members, or coworkers once I transition to a career in web
+              development. I used it daily.
             </Card.Text>
           </Card.Body>
         </Card>
@@ -67,11 +66,11 @@ function Skills() {
         >
           <Card.Header>Header</Card.Header>
           <Card.Body>
-            <Card.Title>SOLOLEARN</Card.Title>
+            <Card.Title>Sololearn</Card.Title>
             <Card.Text>
               Arguably the most useful resource that I employ is an app called
-              SOLOLEARN. I used this app to search for detailed examples of code
-              and a place to apply the code and see if it fit my quandry.
+              Sololearn. I used this app as a place t both search for detailed examples of code
+              and a deploy the code and see if it fit a particular need. The really convenient part of Sololearn is that you can employ code without having to set up the workspace before hand by running any commands at all and thus, it is ideal for the beginning coder.
             </Card.Text>
           </Card.Body>
         </Card>
@@ -85,11 +84,11 @@ function Skills() {
           <Card.Body>
             <Card.Title>Youtube</Card.Title>
             <Card.Text>
-              One of the main resources that I utilized; in regards to learning
+              One of the uselful resources that I utilized; in regards to learning
               the details that I was unable to grasp in the coding program that
-              I attended in 2020 was youtube. Specifically, the JavaScript for
+              I attended in 2020, was Youtube. Specifically, the JavaScript for
               beginners videos by EJ Media; who also has other tutorials
-              centered around coding and learning the specifics of a language.
+              centered around coding and learning the specifics of a language. 
             </Card.Text>
           </Card.Body>
         </Card>
@@ -125,8 +124,8 @@ function Skills() {
             <Card.Text>
               Github is where the work that I did in class is complied for
               future reference by myself or anyone looking into my doings in the
-              web-dev world. Github keeps track of work done, frequency of work
-              done, team projects and group participation and much more.
+              web-dev world. Github aids with the organinzation of a project and keeps track of work done, frequency of work
+              done, team projects, group participation and much, much more.
             </Card.Text>
           </Card.Body>
         </Card>
