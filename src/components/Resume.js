1e25d0a5d5548781e1ec187fa5672c27d95fa983
SS Jones <SSJones.code@gmail.com>
2020-10-14 18:51:27 -0700

    I created the Resume.js component and formatted it inside of a container, row, columns and a jumbotron. Used knowledge garnered from working with Elizabeth Mimms today.

---
+++ src/components/Resume.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { Card, CardGroup, Container } from "react-bootstrap";
+
+export default function Resume(props) {
+  return (
+    <Jumbotron fluid>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Resume</h1>
+            <p>
+              As a unremitting scholar, I am both seeking ways to acquire a
+              knowledge base of and implement novel technologies. I am an ardent
+              full-stack web developer with emphasis on the MERN stack with
+              experience ranging from IT and customer service to full stack web
+              application development. My career objective is to build dynamic
+              webpages from the ground up including the web-design aspect;
+              ultimately, and in doing so build a rapport with clients/customers
+              and to deliver the best product, support, and solutions possible.
+            </p>
+            <h2>Technical SKills</h2>
+            <p>
+              On the front-end I worked with technologies such as HTML, CSS,
+              Materialize, Bootstrap, react-bootstrap, handlebars, React and
+              JavaScript with a strong background in command line interactions
+              or CLI's, GITLAB, GITHUB, deployment with GITHUB pages and Heroku,
+              as well as Netlify to Back-end; Server-side, development using
+              node.js, Express.js, JavaScript, JQuery along with JSX and
+              databases such as MongoDB and MySql as well as AtlasDB. I use the
+              aforementioned in conjunction with knowledge of web-page flow,
+              responsive design, data-structures, Algorithms and storage as well
+              as QA test writing to create an emphasis on functionality and a
+              positive user experience. I strive to create websites that are as
+              aesthetically appealing as they are intuitive and am cognizant of
+              repetitive tasks that can be automated in a new or more efficient
+              way as I am a big-picture thinker with the ability to
+              compartmentalize. I also understand the value of good
+              communication and time management skills and am familiar with
+              Google Docs, Adobe XD, Microsoft Word, Excel, VS CODE, Slack,
+              Hangouts, magento, shopify.
+            </p>
+            <hr/>
+            <h2>Sounds True: 1/2020 – 6/2020</h2>
+            <h3>Customer Service IT support lead</h3>
+            <p>Responsible for complete customer account management.</p>
+            <p>
+              Responsible for assisting customers order track and complete
+              set-up of all purchased audio, digital and hardgoods and to
+              provide patient education.
+            </p>
+            <p>
+              Interacted with customers and provided exemplary customer service.
+            </p>
+            <p>
+              I assisted others in customer service with IT support and acted as
+              liason with the technical and marketing teams to ensure smooth
+              product interaction.
+            </p>
+            <hr/>
+            <h2>Aero Care​: 10/2018 – 1/2020</h2>
+            <h3>Technician/Customer Care</h3>
+            <p>
+              Responsible for customer account management including developing
+              strong relationships with customers, answering customer queries
+              and identifying new business opportunities while ensuring that
+              customers received requested products and services in a timely
+              fashion.
+            </p>
+            <p>
+              Responsible for the delivery, complete set-up, and to provide
+              patient education and pick up of durable medical equipment (DME).
+            </p>
+            <p>
+              Interacted with patients and provided exemplary customer service
+              while remaining calm and professional during times in which
+              patients or caregivers are grieving or upset.
+            </p>
+            <p>
+              Worked closely with the Aero Care customer service team, warehouse
+              staff and other technicians on a daily basis.
+            </p>
+            <p>Tested/troubleshoot equipment issues.</p>
+            <p>
+              Managed and maintained daily route while completing necessary
+              documentation and proactively communicating with patients and
+              office staff during the day as delivery schedule changes arise.
+            </p>
+            <p>
+              Transported deliveries according to manifest in a safe,
+              responsible manner and in accordance with company procedures and
+              applicable DOT regulations.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+  );
+}
