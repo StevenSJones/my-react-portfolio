795d5f931a42d41f200551dadbc304863bc0a119
SS Jones <SSJones.code@gmail.com>
2020-10-15 21:51:29 -0700

    I placed buttons around the links in the nav and also adjusted some css.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Nav } from "react-bootstrap";
+import { Nav, Container, Row, Col, Button } from "react-bootstrap";
+
 //react router dom is imported so that <Router> and <Switch> can be used here and in the Header.js because it houses the buttons.
 
 export default function Header(props) {
@@ -9,95 +9,63 @@ export default function Header(props) {
 
 function Navigation() {
   return (
-    <>
-      <Nav className="color-nav" sticky="top">
-        <Nav.Item>
-          <Link to="/">| Home |</Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Link to="/introduction">Introduction |</Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Link to="/resources">Resources |</Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Link to="/gallery">Gallery |</Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Link to="./resume.html"> Resume |</Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="https://github.com/StevenSJones" target="_blank"> Github |</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="https://www.linkedin.com/in/steven-j-285a33115" target="_blank">
-            {" "}
-            LinkedIn |
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-      {/* <div className="container">
-        <Navbar className="color-nav" sticky="top" collapseOnSelect expand="lg">
-          <div className="row">
-            <div className="col">
-              <Navbar.Brand href="/">
-                <h3 className="text-shadow-light-font-light">Welcome</h3>
-              </Navbar.Brand>
-            </div>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/introduction">
-                  <h3 className="col text-shadow-light-font-light">
-                    Introduction
-                  </h3>
+    <Container className="container-center">
+      <Row>
+        <Col>
+          <Nav className="color-nav">
+            <Nav.Item>
+              <Button className="blended-button">
+                <Nav.Link href="/"> Home </Nav.Link>
+              </Button>
+            </Nav.Item>
+            <Nav.Item>
+              <Button className="blended-button">
+                <Nav.Link href="/introduction"> Introduction </Nav.Link>
+              </Button>
+            </Nav.Item>
+            <Nav.Item>
+              <Button className="blended-button">
+                <Nav.Link href="/resources"> Resources </Nav.Link>
+              </Button>
+            </Nav.Item>
+            <Nav.Item>
+              <Button className="blended-button">
+                <Nav.Link href="/gallery"> Gallery </Nav.Link>
+              </Button>
+            </Nav.Item>
+            <Nav.Item>
+              <Button className="blended-button">
+                <Nav.Link
+                  href="https://docs.google.com/document/d/1F2dCpsvhm61QUFytpRKljhzXUwQ5rVIVUXNMZwSVz9U/edit?usp=sharing"
+                  target="_blank"
+                >
+                  Resume
                 </Nav.Link>
-                <Nav.Link href="/skills">
-                  <h3 className="col text-shadow-light-font-light">
-                    Resources
-                  </h3>
+              </Button>
+            </Nav.Item>
+            <Nav.Item>
+              <Button className="blended-button">
+                <Nav.Link
+                  href="https://github.com/StevenSJones"
+                  target="_blank"
+                >
+                  Github
                 </Nav.Link>
-
-                <Nav.Link href="/gallery">
-                  <h3 className="col text-shadow-light-font-light">Gallery</h3>
+              </Button>
+            </Nav.Item>
+            <Nav.Item>
+              <Button className="blended-button">
+                <Nav.Link
+                  href="https://www.linkedin.com/in/steven-j-285a33115"
+                  target="_blank"
+                >
+                  LinkedIn
                 </Nav.Link>
-                <div className="col justify-content-center">
-                  <NavDropdown
-                    className="text-shadow-light-font-light"
-                    title="More Below"
-                    id="collasible-nav-dropdown"
-                  >
-                    <NavDropdown.Item href="./resume.html">
-                      <h1
-                        className="link-text text-shadow-dark"
-                        target="_blank"
-                      >
-                        Resume
-                      </h1>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="https://github.com/StevenSJones">
-                      <h1
-                        className="link-text text-shadow-dark"
-                        target="_blank"
-                      >
-                        GITHUB
-                      </h1>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="https://www.linkedin.com/in/steven-j-285a33115">
-                      <h1
-                        className="link-text text-shadow-dark"
-                        target="_blank"
-                      >
-                        LinkedIn
-                      </h1>
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </div>
-              </Nav>
-            </Navbar.Collapse>
-          </div>
-        </Navbar>
-      </div> */}
-    </>
+              </Button>
+            </Nav.Item>
+          </Nav>
+        </Col>
+      </Row>
+    </Container>
   );
 }
